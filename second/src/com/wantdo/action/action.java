33d480130b0801e77663bc9983dacf8c698a48c6
Administrator <Administrator@PC-20140526XWZZ>
2014-06-19 15:26:40 +0800

    update

--- second/src/com/wantdo/action/action.java
+++ second/src/com/wantdo/action/action.java
@@ -8,7 +8,7 @@ public class action {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("fwegfsrgetjret");
-		System.out.print(true+flase);
+		System.out.print(true);
 	}
 
 }

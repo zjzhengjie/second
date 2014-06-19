c57bd26a0e68ab4fa5357518eb58b4ca63beb974
Administrator <Administrator@PC-20140526XWZZ>
2014-06-19 15:32:30 +0800

    add

---
+++ src/com/wantdao/java.java
@@ -0,0 +1,13 @@
+package com.wantdao;
+
+public class java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(true);
+	}
+
+}
